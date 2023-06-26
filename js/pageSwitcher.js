ba782b592cfb24e095e2ef5e4434d860e8f6d901
jhon <jenestiven@gmail.com>
2023-06-26 00:24:44 -0500

    Pagina de inicio finalizada (aun dispuesta a mejoras)

--- js/pageSwitcher.js
+++ js/pageSwitcher.js
@@ -8,12 +8,18 @@ class PageSwitcher {
     var startChooser = document.getElementById('startChooser');
     var dbChooser = document.getElementById('dbChooser');
     var geovisorChooser = document.getElementById('geovisorChooser');
+    var geovisorCard = document.getElementById('geovisorCard');
     var aboutChooser = document.getElementById('aboutChooser');
+    var aboutCard = document.getElementById('aboutCard');
+    var dbCard = document.getElementById('dbCard');
     
     startChooser.addEventListener('click', () => this.switchToPage(0));
     dbChooser.addEventListener('click', () => this.switchToPage(1));
     geovisorChooser.addEventListener('click', () => this.switchToPage(2));
     aboutChooser.addEventListener('click', () => this.switchToPage(3));
+    geovisorCard.addEventListener('click', () => this.switchToPage(2));
+    aboutCard.addEventListener('click', () => this.switchToPage(3));
+    dbCard.addEventListener('click', () => this.switchToPage(1));
   }
 
   switchToPage(index) {

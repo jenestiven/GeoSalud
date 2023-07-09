11c9ee8edd45e67a5b7033732388f0968219bd66
jhon <jenestiven@gmail.com>
2023-07-09 14:37:38 -0500

    pagina de base de datos casi terminada, a la espera de agregar la funcionalidad de descarga de tablas

--- Geosalud/public/js/showtable.js
+++ Geosalud/public/js/showtable.js
@@ -2,30 +2,40 @@
 class showTables {
   constructor() {
     this.ips = document.getElementById("ipsTarjet");
-    this.eps = document.getElementById("eps");
-    this.dispensors = document.getElementById("dispensors");
-    this.services = document.getElementById("services");
+    this.eps = document.getElementById("epsTarjet");
+    this.dispensors = document.getElementById("dispensorsTarjet");
+    this.services = document.getElementById("servicesTarjet");
     console.log(this.ips, this.eps, this.dispensors, this.services);
   }
   mostrarIps() {
-    document.getElementById("ipsTarjet").style.visibility = "visible";
+    document.getElementById("ipsTarjet").style.display = "block";
   }
   mostrarEps() {
-    document.getElementById("eps").style.visibility = "visible";
+    document.getElementById("epsTarjet").style.display = "block";
   }
   mostrarDispensors() {
-    document.getElementById("dispensors").style.visibility = "visible";
+    document.getElementById("dispensorsTarjet").style.display = "block";
   }
   mostrarServicios() {
-    document.getElementById("services").style.visibility = "visible";
+    document.getElementById("servicesTarjet").style.display = "block";
   }
   agregarBotones() {
     const divs = [this.ips, this.eps, this.dispensors, this.services];
     divs.forEach(div => {
       const closeButton = document.createElement("button");
       closeButton.textContent = "⇱ Volver al Geoportal";
+      closeButton.style.backgroundColor = "black";
+      closeButton.style.color = "white";
+      closeButton.style.border = "none";
+      closeButton.style.padding = "10px 20px";
+      closeButton.style.borderRadius = "5px";
+      closeButton.style.margin = "10px";
+      closeButton.style.top = "280px";
+      closeButton.style.right = "50px";
+      closeButton.style.cursor = "pointer";
+      closeButton.style.position = "absolute";
       closeButton.addEventListener("click", () => {
-        div.style.visibility = "hidden";
+        div.style.display = "none";
       });
       div.appendChild(closeButton);
     });
